65f486ba83e2e7daf687deb6b4b6958363627196
Mohammad Ali Bazzazi <81759694+bazzazi@users.noreply.github.com>
2021-07-06 21:00:28 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+###############          ##########        #######   #######        #########      #######       #
+#              #        #          #             #         #       #         #           #
+#               #       #          #            #         #        #         #          #        #
+#              #        #          #           #         #         #         #         #         #
+###############         ############          #         #          ###########        #          #
+#              #        #          #         #         #           #         #       #           #
+#               #       #          #        #         #            #         #      #            #
+#              #        #          #       #         #             #         #     #             #
+###############         #          #      #######    #######       #         #    #######        #
+
+# Developer: Mohammad Ali Bazzazi (me)
+
+########################### START ###########################
+# Importing Libraries
 from kivy.app import App 
 from kivy.uix.label import Label 
 
-class TalentOfCoding(App): 
+# Create main App class
+class MyApp(App): 
     def build(self): 
+        # Put a Label in app 
         return Label(text="Hello World", font_size=32) 
 
+# Create instances
 if __name__ == "__main__": 
-    TalentOfCoding().run()
+    MyApp().run()
+########################### END ###########################
